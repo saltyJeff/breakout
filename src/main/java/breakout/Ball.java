4fd011fd7204db7baf0105545d117c02a933977a
saltyJeff <saltyJeff@users.noreply.github.com>
2017-05-08 17:36:30 -0700

    balls

---
+++ src/main/java/breakout/Ball.java
@@ -0,0 +1,15 @@
+package breakout;
+
+public class Ball {
+	private double xPosition;
+	private double yPosition;
+	private double xVelocity;
+	private double yVelocity;
+	
+	public Ball () {
+		xPosition = 10;
+		yPosition = 10;
+		xVelocity = 0;
+		yVelocity = -1;
+	}
+}
